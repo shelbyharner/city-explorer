109a40643b4cc313210a044adbbb1cf75fbc5994
Shelby Harner <shelby.harner@gmail.com>
2021-04-07 20:34:39 -0700

    add city form and component set up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import React from 'react';
 import axios from 'axios';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Header.js';
+import CityForm from './CityForm.js';
+import Footer from './Footer.js';
+
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      alreadySearched: false,
+      locationSearched: '',
+    }
+  }
+
+  handleSearch = () => {
+    this.setState({locationSearched: false});
+  }
+
+  handleLocationSearched = () => {
+    this.setState({
+      alreadySearched: true,
+      locationSearched: locationSearched,
+    });
+  }
+
   render() {
     return(
-      <h1>City Explorer</h1>
+      <div>
+        <Header />
+
+        <CityForm />
+        onClick={this.state.handleLocationSearched}
+
+        <Footer />
+      </div>
     )
   }
 }
