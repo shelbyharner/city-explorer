4084b8f414390146a12fc24012b7d7cc8a430813
Shelby Harner <shelby.harner@gmail.com>
2021-04-14 22:57:39 -0700

    correct lab 7 error component items and deployment

---
+++ src/Weather.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+class Weather extends React.Component {
+  render() {
+    let allListGroupItems = this.props.weatherData.map((day, index) => (
+        <ListGroup.Item key={index}>
+          {day.date}: {day.description}
+        </ListGroup.Item>
+    ))
+    return(
+      <ListGroup>
+        {allListGroupItems}
+      </ListGroup>
+    )
+  }
+}
+
+export default Weather;
